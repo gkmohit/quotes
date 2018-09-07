252b35b7e4bff0524e652c322073ae9b80290be3
Mohit Kishore <gkmohit@gmail.com>
2018-09-07 15:30:43 -0400

    Get quote from favq workd.

--- App.js
+++ App.js
@@ -1,16 +1,74 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, Button, Vibration } from 'react-native';
+import axios from 'axios';
+
 
 export default class App extends React.Component {
+
+  constructor(props) { 
+    super(props)
+    this.state = {
+      quote : {}
+    }
+    this.getQuote = this.getQuote.bind(this);
+  }
+
+  componentDidMount() { 
+    this.getQuote;
+  }
+
   render() {
+
+    const quoteTextStyle = {
+      color: "#000000",
+      fontSize: 35,
+      textAlign: "center"
+    };
+
+    const authorTextStyle = {
+      color: "#000000",
+      fontSize: 25,
+      textAlign: "left"
+    }
+
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <TouchableOpacity style={styles.container} onPress={this.getQuote}>
+        <Text
+         accessibilityLabel={this.state.quote.body}
+         style= {quoteTextStyle}
+        >
+          "{this.state.quote.body}"
+        </Text>
+        <Text
+          accessibilityLabel={this.state.quote.body}
+          style= {authorTextStyle}
+        >
+          - {this.state.quote.author}
+        </Text>
+
+        <Button
+          onPress={this.getQuote}
+          title="Get new Quote"
+          color="#4CAF50"
+          accessibilityLabel="Get new Quote"
+        ></Button>
+      </TouchableOpacity>
     );
   }
+
+  
+  getQuote = () => {
+    axios({
+      method:'get',
+      url:'https://favqs.com/api/qotd',
+    }).then( (response) => {
+        this.setState({
+          quote : response.data.quote 
+        });
+      }).catch( (error) => {
+        console.log(error);
+      });
+  };
 }
 
 const styles = StyleSheet.create({
